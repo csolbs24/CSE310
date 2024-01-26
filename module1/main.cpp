9f5a1012b9369060760fd0246777f576378133a3
Carter <csolbs24@gmail.com>
2024-01-26 15:43:10 -0700

    Make displayBoard fancy

--- module1/main.cpp
+++ module1/main.cpp
@@ -9,11 +9,19 @@ class TicTacToe {
 public:
     char board[3][3];
     void printBoard() {
+        cout << "┏━━━┳━━━┳━━━┓\n";
         for (int i = 0; i < 3; i++) {
+            cout << "┃ ";
             for (int j = 0; j < 3; j++) {
-                cout << board[i][j];
+                cout << board[i][j] << " ┃ ";
             }
             cout << "\n";
+            if (i != 2) {
+                cout << "┣━━━╋━━━╋━━━┫\n";
+            }
+            else {
+                cout << "┗━━━┻━━━┻━━━┛\n";
+            }
         }
     }
     void updateBoard() {
