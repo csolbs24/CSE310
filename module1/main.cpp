726170c0ccbc4a76f17e7bea24e599d42a4fe6b3
Carter <csolbs24@gmail.com>
2024-01-27 00:39:24 -0700

    Add check for if a player has won

--- module1/main.cpp
+++ module1/main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 class TicTacToe {
 public:
+    int turnNumber = 0;
     char board[3][3];
     void printBoard() {
         cout << "┏━━━┳━━━┳━━━┓\n";
@@ -26,6 +27,43 @@ public:
             }
         }
     }
+    void updateBoard(int boardIndex) {
+        board[boardIndex / 3][boardIndex % 3] = ((turnNumber % 2 == 0) ? 'O' : 'X');
+    }
+    bool didWin() {
+        // Check across each row
+        for (int i = 0; i < 3; i++) {
+            if (
+                (board[i][0] == board[i][1]) &&
+                (board[i][0] == board[i][2]) &&
+                (board[i][0] != ' '))
+                return true;
+        }
+
+        // Check across each column
+        for (int i = 0; i < 3; i++) {
+            if (
+                (board[0][i] == board[1][i]) &&
+                (board[0][i] == board[2][i]) &&
+                (board[0][i] != ' '))
+                return true;
+        }
+
+        // Check the diagonals
+        if (
+            (board[0][0] == board[1][1]) &&
+            (board[0][0] == board[2][2]) &&
+            (board[0][0] != ' '))
+            return true;
+        if (
+            (board[0][2] == board[1][1]) &&
+            (board[0][2] == board[2][0]) &&
+            (board[0][2] != ' '))
+            return true;
+
+        // Default means no match
+        return false;
+    }
     TicTacToe() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -37,6 +75,24 @@ public:
 
 int main() {
     TicTacToe ticTacToe = TicTacToe();
-    ticTacToe.printBoard();
+    char playIndexChar;
+    int playIndex;
+    int temp = 0;
+    do {
+        ticTacToe.printBoard();
+        ticTacToe.turnNumber++;
+        cout << ((ticTacToe.turnNumber % 2 == 0) ? "O" : "X");
+        cout << "\'s turn. Type index of where you want to play: ";
+        cin >> playIndexChar;
+        playIndex = playIndexChar - '1';
+        ticTacToe.updateBoard(playIndex);
+
+
+        temp++;
+    }
+    while (!ticTacToe.didWin());
+
+
+
     return 0;
 } 
